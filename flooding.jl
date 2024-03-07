8df69cdf8f3d14e156efe209870fd3044aae13f1
Kostas Andreadis <kandread@umass.edu>
2024-03-07 17:02:31 -0500

    Formatted code

--- flooding.jl
+++ flooding.jl
@@ -1,13 +1,28 @@
 using CSV, DataFrames, DataStructures
-using Rasters, ArchGDAL
+using Rasters
+import ArchGDAL as AG
 
 """
 Read GNSS-R Rongowai observations from CSV `filename`.
 """
 function readRongowai(filename::String)
-    obs = CSV.File(filename;
-                   select=[1, 2, 5, 6, 7, 8, 10],
-                   header=["Lat", "Lon", "", "", "Water", "Fmajor", "Fminor", "Forient", "", "Angle", ""])
+    obs = CSV.File(
+        filename;
+        select = [1, 2, 5, 6, 7, 8, 10],
+        header = [
+            "Lat",
+            "Lon",
+            "",
+            "",
+            "Water",
+            "Fmajor",
+            "Fminor",
+            "Forient",
+            "",
+            "Angle",
+            "",
+        ],
+    )
     obs |> DataFrame
 end
 
@@ -30,7 +45,11 @@ function flooded(filename::String, demfile::Raster)
     ndem = setmappedcrs(view(dem, X(minlon .. maxlon), Y(minlat .. maxlat)), EPSG(4326))
     flood = bathtub(ndem, obs)
     if !isnothing(flood)
-        write(replace(filename, ".txt" => ".tif"), flood, options=Dict("compress" => "LZW"))
+        write(
+            replace(filename, ".txt" => ".tif"),
+            flood,
+            options = Dict("compress" => "LZW"),
+        )
     end
 end
 
@@ -46,7 +65,7 @@ See https://math.stackexchange.com/a/573174 for derivation.
 
 """
 function initFlood(dem::Raster, obs::DataFrame)
-    q = Deque{Tuple{Int, Int}}()
+    q = Deque{Tuple{Int,Int}}()
     nr, nc = size(dem)
     bnds = mappedbounds(dem)
     xmin, xmax = bnds[1]
@@ -56,22 +75,23 @@ function initFlood(dem::Raster, obs::DataFrame)
     lats = mappedindex(dem)[2]
     lons = mappedindex(dem)[1]
     # initialize flood pixel queue
-    for c=1:nrow(obs)
+    for c = 1:nrow(obs)
         if obs.Water[c] == 1
             j = floor(Int, (ymax - obs.Lat[c]) / yres)
             i = floor(Int, (obs.Lon[c] - xmin) / xres)
             if i > 0 && i <= nr && j > 0 && j <= nc
                 push!(q, (i, j))
                 # FIXME: can we use 110 km to convert from 1°?
-                dj = floor(Int, (obs.Fminor[c] / 110000.) / yres)
-                di = floor(Int, (obs.Fmajor[c] / 110000.) / xres)
-                for k=-di:di
-                    for l=-dj:dj
-                        ni = min(max(i+k, 1), nr)
-                        nj = min(max(j+l, 1), nc)
+                dj = floor(Int, (obs.Fminor[c] / 110000.0) / yres)
+                di = floor(Int, (obs.Fmajor[c] / 110000.0) / xres)
+                for k = -di:di
+                    for l = -dj:dj
+                        ni = min(max(i + k, 1), nr)
+                        nj = min(max(j + l, 1), nc)
                         # REVIEW: ellipsoid projection
-                        if !((ni, nj) in q) && (3/4) * (lons[i] - lons[ni])^2 + (lats[j] - lats[nj])^2 < 1
-                        push!(q, (ni, nj))
+                        if !((ni, nj) in q) &&
+                           (3 / 4) * (lons[i] - lons[ni])^2 + (lats[j] - lats[nj])^2 < 1
+                            push!(q, (ni, nj))
                         end
                     end
                 end
@@ -96,30 +116,32 @@ function bathtub(dem::Raster, obs::DataFrame)
         q = initFlood(dem, obs)
         # iterate through queue and flood downstream pixels
         while !isempty(q)
-        i, j = pop!(q)
-        out[i, j] = 1
-        dh = 1.0
-        ci, cj = i, j
-        while dh > 0
-            for k=-1:1
-                for l=-1:1
-                    ni = min(max(ci+k, 1), nr)
-                    nj = min(max(cj+l, 1), nc)
-                    if dem[i, j] > dem[ni, nj] && out[ni, nj] == 0 && dem[ni, nj] != missingval(dem)
-                        push!(q, (ni, nj))
+            i, j = pop!(q)
+            out[i, j] = 1
+            dh = 1.0
+            ci, cj = i, j
+            while dh > 0
+                for k = -1:1
+                    for l = -1:1
+                        ni = min(max(ci + k, 1), nr)
+                        nj = min(max(cj + l, 1), nc)
+                        if dem[i, j] > dem[ni, nj] &&
+                           out[ni, nj] == 0 &&
+                           dem[ni, nj] != missingval(dem)
+                            push!(q, (ni, nj))
+                        end
                     end
                 end
+                i1, i2 = max(ci - 1, 1), min(ci + 1, nr)
+                j1, j2 = max(cj - 1, 1), min(cj + 1, nc)
+                dh = maximum(dem[ci, cj] .- dem[i1:i2, j1:j2])
+                n = argmax(dem[ci, cj] .- dem[i1:i2, j1:j2])
+                ci = collect(i1:i2)[n[1]]
+                cj = collect(j1:j2)[n[2]]
             end
-            i1, i2 = max(ci-1, 1), min(ci+1, nr)
-            j1, j2 = max(cj-1, 1), min(cj+1, nc)
-            dh = maximum(dem[ci, cj] .- dem[i1:i2, j1:j2])
-            n = argmax(dem[ci, cj] .- dem[i1:i2, j1:j2])
-            ci = collect(i1:i2)[n[1]]
-            cj = collect(j1:j2)[n[2]]
+            println(length(q))
         end
-        println(length(q))
-        end
-        rout = Raster(out, dims(dem), missingval=0)
+        rout = Raster(out, dims(dem), missingval = 0)
     else
         rout = nothing
     end
